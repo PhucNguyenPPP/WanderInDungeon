8284e87e728425697bc07d4e8bee1c8c35c07841
minhlocdev <91857852+minhlocdev@users.noreply.github.com>
2024-06-02 02:12:45 +0700

    add Coin Manager + Weapons for selected Player

---
+++ Assets/Scripts/Manager/CoinManager.cs
@@ -0,0 +1,35 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CoinManager : PersistentSingleton<CoinManager>
+{
+    [Header("Config")]
+    [SerializeField] private float initialCoinsTest;
+    
+    public float Coins { get; private set; }
+    private const string COIN_KEY = "Coins";
+
+    private void Start()
+    {
+        Coins = PlayerPrefs.GetFloat(COIN_KEY, initialCoinsTest);
+    }
+
+    public void AddCoins(float amount)
+    {
+        Coins += amount;
+        PlayerPrefs.SetFloat(COIN_KEY, Coins);
+        PlayerPrefs.Save();
+    }
+
+    public void RemoveCoins(float amount)
+    {
+        if (Coins >= amount)
+        {
+            Coins -= amount;
+            PlayerPrefs.SetFloat(COIN_KEY, Coins);
+            PlayerPrefs.Save();
+        }
+    }
+}
