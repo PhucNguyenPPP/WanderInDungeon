0610e93f2b8cfa28b09357e2969b0911460ab2a7
minhlocdev <91857852+minhlocdev@users.noreply.github.com>
2024-06-02 18:57:30 +0700

    fix lost script object

---
+++ Assets/Scripts/Enemy/EnemyHealth.cs
@@ -0,0 +1,52 @@
+using System;
+using System.Collections;
+using UnityEngine;
+
+public class EnemyHealth : MonoBehaviour, ITakeDamage
+{
+    [Header("Config")]
+    [SerializeField] private float health;
+
+    private SpriteRenderer sp;
+    private float enemyHealth;
+    private Color initialColor;
+    private Coroutine colorCoroutine;
+
+    private void Awake()
+    {
+        sp = GetComponent<SpriteRenderer>();
+    }
+
+    private void Start()
+    {
+        enemyHealth = health;
+        initialColor = sp.color;
+    }
+
+    public void TakeDamage(float amount)
+    {
+        enemyHealth -= amount;
+        ShowDamageColor();
+        if (enemyHealth <= 0)
+        {
+            Destroy(gameObject);
+        }
+    }
+
+    private void ShowDamageColor()
+    {
+        if (colorCoroutine != null)
+        {
+            StopCoroutine(colorCoroutine);
+        }
+
+        colorCoroutine = StartCoroutine(IETakeDamage());
+    }
+    
+    private IEnumerator IETakeDamage()
+    {
+        sp.color = Color.red;
+        yield return new WaitForSeconds(0.1f);
+        sp.color = initialColor;
+    }
+}
